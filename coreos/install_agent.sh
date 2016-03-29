171560d1fe39a477218b7407efa6bdfa002877a4
megastef <stefan.thies@sematext.com>
2016-03-29 14:43:30 +0200

    installer for SPM only

--- coreos/install_agent.sh
+++ coreos/install_agent.sh
@@ -13,9 +13,17 @@ if [[ -n "$1" && -n "$2" && -n "$3"  ]] ; then
 	fleetctl load sematext-agent.service; fleetctl start sematext-agent.service
 	wget https://raw.githubusercontent.com/sematext/sematext-agent-docker/master/coreos/logsene.service
 	fleetctl load logsene.service; fleetctl start logsene.service;
-
 else 
-	echo "Missing paramaters. Usage:"
-	echo "install_agent.sh SPM_TOKEN LOGSENE_TOKEN LOGSENE_GATEWAY_PORT (e.g. 9000)"
-	echo "Please optain your access tokens here: https://apps.sematext.com/"
+	if [[ $# -eq 1 ]] ; then 
+	  export SPM_TOKEN=$1
+	  echo $SPM_TOKEN
+	  etcdctl set /sematext.com/myapp/spm/token $SPM_TOKEN
+	  etcdctl set /sematext.com/myapp/logsene/gateway_port 9000
+	  wget https://raw.githubusercontent.com/sematext/sematext-agent-docker/master/coreos/sematext-agent.service
+	  fleetctl load sematext-agent.service; fleetctl start sematext-agent.service
+    else
+	  echo "Missing paramaters. Usage:"
+	  echo "install_agent.sh SPM_TOKEN LOGSENE_TOKEN LOGSENE_GATEWAY_PORT (e.g. 9000)"
+	  echo "Please obtain your access tokens here: https://apps.sematext.com/"
+    fi
 fi 
