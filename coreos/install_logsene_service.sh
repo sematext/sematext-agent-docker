74694c2c280cd241337185cf86b5aa0de63a7b4d
megastef <stefan.thies@sematext.com>
2016-05-26 16:22:46 +0200

    add logsene only install script for CoreOS

--- coreos/install_logsene_service.sh
+++ coreos/install_logsene_service.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ -n "$1" && -n "$2" ]] ; then 
-	export LOGSENE_TOKEN=$2
-	export LOGSENE_GATEWAY_PORT=$3
+	export LOGSENE_TOKEN=$1
+	export LOGSENE_GATEWAY_PORT=$2
 	etcdctl set /sematext.com/myapp/logsene/token $LOGSENE_TOKEN
 	etcdctl set /sematext.com/myapp/logsene/gateway_port $LOGSENE_GATEWAY_PORT
-	
+
 	wget https://raw.githubusercontent.com/sematext/sematext-agent-docker/master/coreos/sematext-agent.service
 	fleetctl load sematext-agent.service; fleetctl start sematext-agent.service
 	wget https://raw.githubusercontent.com/sematext/sematext-agent-docker/master/coreos/logsene.service
