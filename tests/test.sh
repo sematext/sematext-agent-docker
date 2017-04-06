e377e5271e783ba8bf5d622b1e9fcf2385103bcc
megastef <s.thies@bigdata-analyst.de>
2017-04-06 22:29:34 +0200

    add build command to travis test

--- tests/test.sh
+++ tests/test.sh
@@ -15,14 +15,10 @@ function log_count_test ()
 	export NGINX_PORT=9998
   docker rm -f sematext-agent > /dev/null
 	docker rm -f nginx1  > /dev/null
-  docker run -d --name sematext-agent -e LOGSENE_TOKEN=$LOGSENE_TOKEN -e SPM_TOKEN=$SPM_TOKEN -v /var/run/docker.sock:/var/run/docker.sock sematext/sematext-agent-docker:test
+  docker run -d --name sematext-agent -e ENABLE_LOGSENE_STATS="true" -e MATCH_BY_IMAGE="nginx" -e LOGSENE_TOKEN=$LOGSENE_TOKEN -e SPM_TOKEN=$SPM_TOKEN -v /var/run/docker.sock:/var/run/docker.sock sematext/sematext-agent-docker:test
   docker run -d --name nginx1 -p $NGINX_PORT:80 nginx  
-
-	export LOG_NO=5
-	docker run --rm -t --net=host jstarcher/siege -r $LOG_NO -c 50 http://127.0.0.1:$NGINX_PORT/${TEST_ID} | grep Transactions
-	sleep 90 
-	docker rm -f sematext-agent > /dev/null
-	docker rm -f nginx1  > /dev/null
+	docker run --rm -t --net=host jstarcher/siege -r 8 -c 25 http://127.0.0.1:$NGINX_PORT/${TEST_ID} | grep Transactions
+	sleep 120 
 	echo '{"query" : { "query_string" : {"query": "path:'$TEST_ID' AND status_code:404" }}}' > query.txt
 	echo curl -XPOST "https://logsene-receiver.sematext.com/$LOGSENE_TOKEN/_count" -d @query.txt
 	echo Elasticsearch Query: 
@@ -30,7 +26,7 @@ function log_count_test ()
 	export count=$(curl -XPOST "logsene-receiver.sematext.com/$LOGSENE_TOKEN/_count" -d @query.txt | jq '.count')
 	echo "log count in Logsene: $count"
 	# each nginx request generates 2 logs
-	export generated_logs=$(expr $LOG_NO \* 50 \* 2)
+	export generated_logs=200
 	echo $generated_logs
 	export result=$(expr $count  - $generated_logs)
 	if [ $result == 0 ]; then
