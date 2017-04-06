64a514922d3a55f46fad4460a6b77584fa954708
megastef <s.thies@bigdata-analyst.de>
2017-04-06 21:20:08 +0200

    remove docker-compose from travis test

--- tests/test.sh
+++ tests/test.sh
@@ -6,16 +6,17 @@ fi
 if [ -f "env.sh" ]; then
   source ./env.sh
 fi
-# read nginx port from docker-compose file
-export PORT=$(cat ./docker-compose.yml | grep :80 | awk '{print $2}' | tr : ' ' | awk '{print $1}')
-docker-compose up -d 
+export NGINX_PORT=9998
+# docker rm -f sematext-agent
+docker run -d --name sematext-agent -e LOGSNE_TOKEN -e SPM_TOKEN -v /var/run/docker.sock:/var/run/docker.sock sematext/sematext-agent-docker:test
+docker run -d -p $NGINX_PORT:80 nginx  
 function log_count_test () 
 {
 	# generate random test ID
 	export TEST_ID=TEST$(awk -v min=1000 -v max=900000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')$(awk -v min=1000 -v max=900000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
 	echo testID = $TEST_ID
 	export LOG_NO=5
-	docker run --rm -t --net=host jstarcher/siege -r $LOG_NO -c 50 http://127.0.0.1:$PORT/${TEST_ID} | grep Transactions
+	docker run --rm -t --net=host jstarcher/siege -r $LOG_NO -c 50 http://127.0.0.1:$NGINX_PORT/${TEST_ID} | grep Transactions
 	sleep 40 
 	echo '{"query" : { "query_string" : {"query": "path:'$TEST_ID' AND status_code:404" }}}' > query.txt
 	echo curl -XPOST "https://logsene-receiver.sematext.com/LOGSENE_TOKEN/_count?q=path:?$TEST_ID" -d @query.txt
