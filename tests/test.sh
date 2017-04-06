86d5b03d146f22b5d90770f621ff0eff868dc7d5
megastef <s.thies@bigdata-analyst.de>
2017-04-06 21:53:12 +0200

    update test script

--- tests/test.sh
+++ tests/test.sh
@@ -6,21 +6,28 @@ fi
 if [ -f "env.sh" ]; then
   source ./env.sh
 fi
-export NGINX_PORT=9998
-# docker rm -f sematext-agent
-docker run -d --name sematext-agent -e LOGSENE_TOKEN=$LOGSENE_TOKEN -e SPM_TOKEN=$SPM_TOKEN -v /var/run/docker.sock:/var/run/docker.sock sematext/sematext-agent-docker:test
-docker run -d -p $NGINX_PORT:80 nginx  
+
 function log_count_test () 
 {
 	# generate random test ID
 	export TEST_ID=TEST$(awk -v min=1000 -v max=900000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')$(awk -v min=1000 -v max=900000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
 	echo testID = $TEST_ID
+	export NGINX_PORT=9998
+  docker rm -f sematext-agent > /dev/null
+	docker rm -f nginx1  > /dev/null
+  docker run -d --name sematext-agent -e LOGSENE_TOKEN=$LOGSENE_TOKEN -e SPM_TOKEN=$SPM_TOKEN -v /var/run/docker.sock:/var/run/docker.sock sematext/sematext-agent-docker:test
+  docker run -d --name nginx1 -p $NGINX_PORT:80 nginx  
+
 	export LOG_NO=5
 	docker run --rm -t --net=host jstarcher/siege -r $LOG_NO -c 50 http://127.0.0.1:$NGINX_PORT/${TEST_ID} | grep Transactions
-	sleep 60 
+	sleep 90 
+	docker rm -f sematext-agent > /dev/null
+	docker rm -f nginx1  > /dev/null
 	echo '{"query" : { "query_string" : {"query": "path:'$TEST_ID' AND status_code:404" }}}' > query.txt
-	echo curl -XPOST "https://logsene-receiver.sematext.com/LOGSENE_TOKEN/_count?q=path:?$TEST_ID" -d @query.txt
-	export count=$(curl -XPOST "logsene-receiver.sematext.com/$LOGSENE_TOKEN/_count?q=path:?$TEST_ID" -d @query.txt | jq '.count')
+	echo curl -XPOST "https://logsene-receiver.sematext.com/$LOGSENE_TOKEN/_count" -d @query.txt
+	echo Elasticsearch Query: 
+	cat query.txt
+	export count=$(curl -XPOST "logsene-receiver.sematext.com/$LOGSENE_TOKEN/_count" -d @query.txt | jq '.count')
 	echo "log count in Logsene: $count"
 	# each nginx request generates 2 logs
 	export generated_logs=$(expr $LOG_NO \* 50 \* 2)
